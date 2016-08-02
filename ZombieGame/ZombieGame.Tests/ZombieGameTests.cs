6b72a10e9230813b6d77a58ef0548f1aed3e92be
Marcin_Rogalski <marcin_rogalski@epam.com>
2016-08-02 08:57:42 +0200

    Added 2 unit tests

---
+++ ZombieGame/ZombieGame.Tests/ZombieGameTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZombieGame.Tests
+{
+	[TestClass()]
+	public class ZombieGameTests
+	{
+		[TestMethod]
+		public void ZombieCount_GreaterThanZero_Success()
+		{
+			int zombie = 10;
+
+			Assert.IsTrue(zombie > 0);
+		}
+
+		[TestMethod]
+		public void Player_ExistsOnPage_Success()
+		{
+			int playerId = 1;
+
+			Assert.IsNotNull(playerId);
+		}
+	}
+}
